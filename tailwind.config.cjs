f62f5bd23c4a400fa4e96d758299970e260cf249
Leo Katzengruber <leofel.katz@gmail.com>
2023-08-19 13:28:40 +0200

    create layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        fontFamily: {
+            sans: ["Satoshi-Variable", "ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
+        },
+        colors: {
+            transparent: "rgba(0, 0, 0, 0)",
+            white: "#FFFFFF",
+            black: "#1C1C1C",
+        },
+        extend: {},
+    },
+    plugins: [],
 }
