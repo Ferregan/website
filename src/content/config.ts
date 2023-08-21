54c3e9bfdfd49ffa2c09706ca782bd30fd3cc72b
Leo Katzengruber <leofel.katz@gmail.com>
2023-08-21 18:24:08 +0200

    add vaheh content, next step: fix layouting issues

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,11 +2,12 @@ import { defineCollection, z } from 'astro:content';
 
 const portfolioCollection = defineCollection({
     type: "content",
-    schema: z.object({
+    schema: ({ image }) => z.object({
         title: z.string(),
         order: z.number(),
         date: z.string(),
         services: z.array(z.string()),
+        banner: image(),
     }),
 });
 
